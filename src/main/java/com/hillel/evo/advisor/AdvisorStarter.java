857691c57e10a9871b1f237c605daed699a227b1
Zhurylo Mykhailo <dir2000@ukr.net>
2019-10-07 20:23:18 +0300

    change build.gradle, add AdvisorStarter class

---
+++ src/main/java/com/hillel/evo/advisor/AdvisorStarter.java
@@ -0,0 +1,11 @@
+package com.hillel.evo.advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(AdvisorStarter.class, args);
+    }
+}
