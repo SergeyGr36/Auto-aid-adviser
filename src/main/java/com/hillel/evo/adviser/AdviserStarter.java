249ea008747d7c239ec2aca472d65badbc9bde2d
Zhurylo Mykhailo <dir2000@ukr.net>
2019-10-08 19:47:18 +0300

    add jacoco and pmd plugins

--- src/main/java/com/hillel/evo/adviser/AdviserStarter.java
+++ src/main/java/com/hillel/evo/adviser/AdviserStarter.java
@@ -1,4 +1,4 @@
-package com.hillel.evo.advisor;
+package com.hillel.evo.adviser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdviserStarter {
     public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
         SpringApplication.run(AdviserStarter.class, args);
     }
 }
