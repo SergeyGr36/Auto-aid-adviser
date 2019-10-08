67e2049be1f72cf789864ca0a7bab42b048e3b28
Zhurylo Mykhailo <dir2000@ukr.net>
2019-10-08 10:38:06 +0300

    rename starter class

--- src/main/java/com/hillel/evo/advisor/AdviserStarter.java
+++ src/main/java/com/hillel/evo/advisor/AdviserStarter.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdvisorStarter {
+public class AdviserStarter {
     public static void main(String[] args) {
-        SpringApplication.run(AdvisorStarter.class, args);
+        SpringApplication.run(AdviserStarter.class, args);
     }
 }
