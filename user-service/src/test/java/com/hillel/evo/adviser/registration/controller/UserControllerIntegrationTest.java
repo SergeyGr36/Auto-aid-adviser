6d958b51b5de79e72f4668a589bb72c29a2b93ad
GlazkovAndrey <gan30101983@gmail.com>
2019-10-18 22:41:29 +0300

    rename module

--- user-service/src/test/java/com/hillel/evo/adviser/registration/controller/UserControllerIntegrationTest.java
+++ user-service/src/test/java/com/hillel/evo/adviser/registration/controller/UserControllerIntegrationTest.java
@@ -1,8 +1,5 @@
 package com.hillel.evo.adviser.registration.controller;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hillel.evo.adviser.registration.dto.RegistrationDto;
 import com.hillel.evo.adviser.registration.dto.UserDto;
 import com.hillel.evo.adviser.registration.enums.RoleUser;
@@ -12,7 +9,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
