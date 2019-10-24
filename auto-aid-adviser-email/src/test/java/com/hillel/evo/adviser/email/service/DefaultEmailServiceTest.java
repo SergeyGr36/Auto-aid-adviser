5cd5162db8735f1a579ecfa8123bb27579b98adf
Zhurylo Mykhailo <dir2000@ukr.net>
2019-10-24 19:41:55 +0300

    add EmailApplication class

--- auto-aid-adviser-email/src/test/java/com/hillel/evo/adviser/email/service/DefaultEmailServiceTest.java
+++ auto-aid-adviser-email/src/test/java/com/hillel/evo/adviser/email/service/DefaultEmailServiceTest.java
@@ -1,38 +1,22 @@
 package com.hillel.evo.adviser.email.service;
 
 import com.hillel.evo.adviser.email.dto.MessageDto;
-//import org.junit.jupiter.api.Test;
-
-import org.junit.Test;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import static org.mockito.Mockito.mock;
 
-@SpringBootTest//(classes = {DefaultEmailServiceTest.MyTestConfiguration.class})
-@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DefaultEmailServiceTest {
     //private static final JavaMailSender mockSender = mock(JavaMailSender.class);
-//    @Autowired
-//    EmailService service;// = new DefaultEmailService(new JavaMailSenderImpl());
-//    @Test
-//    public void sendMessage() {
-//        System.out.println("Service: " + service);
-//        MessageDto dto = new MessageDto.Builder().setToAddresses("dir2000@ukr.net").build();
-//        service.sendMessage(dto);
-//    }
-
-//    @TestConfiguration
-//    @ComponentScan()
-//    class MyTestConfiguration {
-//
-//    }
+    @Autowired
+    EmailService service;
+    @Test
+    public void sendMessage() {
+        System.out.println("Service: " + service);
+        MessageDto dto = new MessageDto.Builder().setToAddresses("dir2000@ukr.net").build();
+        service.sendMessage(dto);
+    }
 }
