492f5c00be27f94f95fe475e78a02ca9c2f7b2cb
oleg_kosenkov <o.kosenkov357@gmail.com>
2019-10-31 22:07:23 +0200

    regexp in LoginRequestDto corrected

--- auto-aid-adviser-security/src/main/java/com/hillel/evo/adviser/dto/LoginRequestDto.java
+++ auto-aid-adviser-security/src/main/java/com/hillel/evo/adviser/dto/LoginRequestDto.java
@@ -10,15 +10,11 @@ import javax.validation.constraints.Size;
 @Data
 public class LoginRequestDto {
 
-    @Email()
-    @NotBlank()
-    @Pattern(regexp="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message="Invalid email format")
+    @Email(regexp = "^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*$", message = "Invalid email format")
     @Size(min = 6, max = 40)
     private String email;
 
-    @NotBlank()
-    @Pattern(regexp="[A-Za-z0-9_-]+", message="Invalid password format")
+    @Pattern(regexp="[\\w-]+", message="Invalid password format")
     @Size(min = 8, max = 64)
     private String password;
-
 }
