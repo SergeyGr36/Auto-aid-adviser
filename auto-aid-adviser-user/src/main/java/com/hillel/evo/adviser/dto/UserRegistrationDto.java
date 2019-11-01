f07b5b16c69f1381d0edd06174be0791bfc5aa8d
oleg_kosenkov <o.kosenkov357@gmail.com>
2019-11-01 23:11:02 +0200

    Constraint @NotEmpty removed from registration dto

--- auto-aid-adviser-user/src/main/java/com/hillel/evo/adviser/dto/UserRegistrationDto.java
+++ auto-aid-adviser-user/src/main/java/com/hillel/evo/adviser/dto/UserRegistrationDto.java
@@ -4,22 +4,18 @@ import com.hillel.evo.adviser.enums.RoleUser;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
 public class UserRegistrationDto {
 
-    @NotEmpty
     @Email(regexp = "^[\\w-\\.]+@[\\w-\\.]+$", message = "Violation of email format")
     @Size(min = 3, max = 128)
     private String email;
 
-    @NotEmpty
     @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[\\w]{8,64}", message = "Violation of password format")
     private String password;
 
-    @NotEmpty
     private RoleUser role;
 }
