1d5c8aac868f2d15e20080e1a8ed55908733385a
oleg_kosenkov <o.kosenkov357@gmail.com>
2019-11-01 22:59:59 +0200

    Part of registration test

--- auto-aid-adviser-main/src/main/java/com/hillel/evo/adviser/controller/RegistrationController.java
+++ auto-aid-adviser-main/src/main/java/com/hillel/evo/adviser/controller/RegistrationController.java
@@ -7,13 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 import static org.springframework.http.HttpStatus.OK;
 
-@RestController("/")
+@RestController()
+@RequestMapping("/")
 public class RegistrationController {
 
     private final transient RegistrationService registrationService;
@@ -22,7 +24,20 @@ public class RegistrationController {
         this.registrationService = registrationService;
     }
 
-    @PostMapping("user/register")
+    /**
+     * Registers a new user with provided email, password and role.
+     *
+     * Email string should be a well formed email.
+     * Password should be 8-64 symbols long, could contain uppercase and lowercase latin letters,
+     * digits 0-9, and underscores.
+     * Should contain at least one digit, one uppercase and one lowercase letter.
+     * Violation of these rules results in "bad request" status.
+     *
+     * If the user with provided email already exist, the returned status is "
+     * @param registrationDto
+     * @return
+     */
+    @PostMapping("/register")
     public ResponseEntity<AdviserUserDetailsDto> registerUser(
             @RequestBody @Valid UserRegistrationDto registrationDto) {
 
@@ -30,7 +45,15 @@ public class RegistrationController {
     }
 
 
-    @PostMapping("user/activate/{activationCode}")
+    /**
+     * Activates a new user.
+     * Returns a response with status ok, jwt token in "Authorization" header, and user dto body.
+     * Jwt holds user id.
+     *
+     * @param activationCode the activation code from email.
+     * @return Returns a response with status ok, jwt token in "Authorization" header, and user dto body.
+     */
+    @PostMapping("/activate/{activationCode}")
     public ResponseEntity<AdviserUserDetailsDto> activateUser(
             @PathVariable String activationCode) {
 
