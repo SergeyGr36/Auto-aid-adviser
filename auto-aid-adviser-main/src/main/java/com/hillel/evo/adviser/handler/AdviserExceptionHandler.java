62858d2f1f70b9da9f7c294fe4e481d1f46d4110
oleg_kosenkov <o.kosenkov357@gmail.com>
2019-11-04 13:34:05 +0200

    Constraint validation handler added.

--- auto-aid-adviser-main/src/main/java/com/hillel/evo/adviser/handler/AdviserExceptionHandler.java
+++ auto-aid-adviser-main/src/main/java/com/hillel/evo/adviser/handler/AdviserExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.hillel.evo.adviser.handler;
 
-import com.hillel.evo.adviser.exception.AccessTokenExpiredException;
-import com.hillel.evo.adviser.exception.InvalidJwtTokenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,17 +14,5 @@ public class AdviserExceptionHandler {
     protected ResponseEntity<String> malformedParameterHandler(Exception e) {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Constraint violation");
-    }
-
-    @ExceptionHandler(AccessTokenExpiredException.class)
-    protected ResponseEntity<String> expiredTokenHandler(Exception e) {
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Session expired");
-    }
-
-    @ExceptionHandler(InvalidJwtTokenException.class)
-    protected ResponseEntity<String> invalidTokenHandler(Exception e) {
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
     }
 }
