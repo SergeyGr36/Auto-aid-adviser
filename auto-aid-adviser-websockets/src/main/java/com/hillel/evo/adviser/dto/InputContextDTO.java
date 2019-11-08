dce8ae8d4dbddaceacd899de37a75a86baf77f4f
ariabchuk <andrew.ryabchuk@gmail.com>
2019-11-08 12:10:58 +0200

    websocket backend

---
+++ auto-aid-adviser-websockets/src/main/java/com/hillel/evo/adviser/dto/InputContextDTO.java
@@ -0,0 +1,9 @@
+package com.hillel.evo.adviser.dto;
+
+import lombok.Data;
+
+@Data
+public class InputContextDTO {
+
+    private String context;
+}
---
+++ auto-aid-adviser-websockets/src/main/java/com/hillel/evo/adviser/dto/OutputContextListDTO.java
@@ -0,0 +1,11 @@
+package com.hillel.evo.adviser.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class OutputContextListDTO {
+
+    List<String> contextList;
+}
