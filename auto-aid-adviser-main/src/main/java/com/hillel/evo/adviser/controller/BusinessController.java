2c6c8431a15b80426499dcd88b122d255018e683
GlazkovAndrey <gan30101983@gmail.com>
2019-11-11 14:59:57 +0200

    merge with master

--- auto-aid-adviser-main/src/main/java/com/hillel/evo/adviser/controller/BusinessController.java
+++ auto-aid-adviser-main/src/main/java/com/hillel/evo/adviser/controller/BusinessController.java
@@ -5,11 +5,13 @@ import com.hillel.evo.adviser.dto.BusinessDto;
 import com.hillel.evo.adviser.enums.RoleUser;
 import com.hillel.evo.adviser.repository.AdviserUserDetailRepository;
 import com.hillel.evo.adviser.service.BusinessService;
+import com.hillel.evo.adviser.service.SecurityUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,41 +42,42 @@ public class BusinessController {
 
     @Secured(ROLE_BUSINESS)
     @GetMapping
-    public ResponseEntity<List<BusinessDto>> getBusiness(Principal principal){
-        Long userId = getUserIdByMail(principal.getName());
+    public ResponseEntity<List<BusinessDto>> getBusiness(Authentication authentication){
+        Long userId = getUserFromAuthentication(authentication);
         return new ResponseEntity<>(businessService.findAllByUser(userId), HttpStatus.OK);
     }
 
     @Secured(ROLE_BUSINESS)
     @GetMapping("/{id}")
-    public ResponseEntity<BusinessDto> findBusinessById(@PathVariable Long id, Principal principal){
-        Long userId = getUserIdByMail(principal.getName());
+    public ResponseEntity<BusinessDto> findBusinessById(@PathVariable Long id, Authentication authentication){
+        Long userId = getUserFromAuthentication(authentication);
         return ResponseEntity.ok(businessService.findBusinessById(id, userId));
     }
 
     @Secured(ROLE_BUSINESS)
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<BusinessDto> createBusiness(@Validated @RequestBody final BusinessDto businessDTO, Principal principal){
-        Long userId = getUserIdByMail(principal.getName());
+    public ResponseEntity<BusinessDto> createBusiness(@Validated @RequestBody final BusinessDto businessDTO, Authentication authentication){
+        Long userId = getUserFromAuthentication(authentication);
         return ResponseEntity.ok(businessService.createBusiness(businessDTO, userId));
     }
 
     @Secured(ROLE_BUSINESS)
     @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<BusinessDto> updateBusiness(@Validated @RequestBody final BusinessDto businessDTO, Principal principal){
-        Long userId = getUserIdByMail(principal.getName());
+    public ResponseEntity<BusinessDto> updateBusiness(@Validated @RequestBody final BusinessDto businessDTO, Authentication authentication){
+        Long userId = getUserFromAuthentication(authentication);
         return ResponseEntity.ok(businessService.updateBusiness(businessDTO, userId));
     }
 
     @Secured(ROLE_BUSINESS)
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteBusiness(@PathVariable Long id, Principal principal){
-        Long userId = getUserIdByMail(principal.getName());
+    public ResponseEntity<String> deleteBusiness(@PathVariable Long id, Authentication authentication){
+        Long userId = getUserFromAuthentication(authentication);
         businessService.deleteBusiness(id, userId);
         return new ResponseEntity<String>("Deleted successful", HttpStatus.OK);
     }
 
-    private Long getUserIdByMail(String mail) {
-       return userRepo.findByEmail(mail).get().getId();
+    private Long getUserFromAuthentication(Authentication authentication) {
+        SecurityUserDetails userDetails = (SecurityUserDetails) authentication.getPrincipal();
+        return userDetails.getUserId();
     }
 }
