00ffd6d697b6d0b2dd020e205212d2af038a2eea
Zhurylo Mykhailo <dir2000@ukr.net>
2019-11-12 19:59:11 +0200

    add prepare-image.sql

--- auto-aid-adviser-image/src/main/java/com/hillel/evo/adviser/entity/Image.java
+++ auto-aid-adviser-image/src/main/java/com/hillel/evo/adviser/entity/Image.java
@@ -5,6 +5,8 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
@@ -14,7 +16,9 @@ import javax.validation.constraints.NotNull;
 @EqualsAndHashCode(of = {"id"})
 public class Image {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotNull
     private String keyFileName;
 
--- auto-aid-adviser-image/src/test/java/com/hillel/evo/adviser/service/DefaultImageServiceTest.java
+++ auto-aid-adviser-image/src/test/java/com/hillel/evo/adviser/service/DefaultImageServiceTest.java
@@ -1,13 +1,27 @@
 package com.hillel.evo.adviser.service;
 
+import com.hillel.evo.adviser.ImageApplication;
+import com.hillel.evo.adviser.entity.Image;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest(classes = {ImageApplication.class})
+@Sql(value = {"/prepare-image.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class DefaultImageServiceTest {
+    @Autowired
+    private ImageService imageService;
 
     @Test
-    void create() {
+    void whenCreateImageShouldCreateImage() {
+        Image imageToSave = new Image("filename.jpg");
+
+        Image savedImage = imageService.create(imageToSave);
+
+        assertNotNull(savedImage.getId());
     }
 
     @Test
@@ -16,6 +30,7 @@ class DefaultImageServiceTest {
 
     @Test
     void findById() {
+
     }
 
     @Test
