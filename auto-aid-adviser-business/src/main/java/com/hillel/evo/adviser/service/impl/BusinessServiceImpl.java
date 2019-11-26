ee9c3ae07a99dfb3cb3f94909c49beea0b7b21b2
GlazkovAndrey <gan30101983@gmail.com>
2019-11-27 00:19:34 +0200

    some fix

--- auto-aid-adviser-business/src/main/java/com/hillel/evo/adviser/service/impl/BusinessServiceImpl.java
+++ auto-aid-adviser-business/src/main/java/com/hillel/evo/adviser/service/impl/BusinessServiceImpl.java
@@ -21,10 +21,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class BusinessServiceImpl implements BusinessService {
