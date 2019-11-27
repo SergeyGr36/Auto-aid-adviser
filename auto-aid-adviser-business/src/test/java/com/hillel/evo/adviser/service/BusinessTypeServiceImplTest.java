c327d67764f276f05ad29c2b4172c744864e299a
GlazkovAndrey <gan30101983@gmail.com>
2019-11-27 11:04:56 +0200

    some fix

--- auto-aid-adviser-business/src/test/java/com/hillel/evo/adviser/service/BusinessTypeServiceImplTest.java
+++ auto-aid-adviser-business/src/test/java/com/hillel/evo/adviser/service/BusinessTypeServiceImplTest.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {BusinessApplication.class})
-@Sql(value = {"/clean-business.sql", "/clean-user.sql", "/create-user.sql", "/create-business.sql"},
+@Sql(value = {"/create-user.sql", "/create-business.sql"},
         executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(value = {"/clean-business.sql", "/clean-user.sql"},
         executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
