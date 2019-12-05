5f0141ff2c9460b021bc9382cad9e63c871bea7b
GlazkovAndrey <gan30101983@gmail.com>
2019-12-05 21:25:00 +0200

    test build

--- auto-aid-adviser-image/src/test/java/com/hillel/evo/adviser/service/DefaultImageServiceTest.java
+++ auto-aid-adviser-image/src/test/java/com/hillel/evo/adviser/service/DefaultImageServiceTest.java
@@ -17,7 +17,10 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
