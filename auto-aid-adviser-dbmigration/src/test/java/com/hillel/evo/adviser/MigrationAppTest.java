07919adf27de359a5778df521e860170edbef24c
ariabchuk <andrew.ryabchuk@gmail.com>
2019-12-28 21:36:27 +0200

    added empty test

---
+++ auto-aid-adviser-dbmigration/src/test/java/com/hillel/evo/adviser/MigrationAppTest.java
@@ -0,0 +1,15 @@
+package com.hillel.evo.adviser;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MigrationApp.class)
+public class MigrationAppTest {
+
+    @Test
+    @Disabled
+    public void testMigration() {
+
+    }
+}
