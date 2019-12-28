26bb032de2453a80e5cb513f91353d5f20c54e8d
GlazkovAndrey <gan30101983@gmail.com>
2019-12-28 15:01:48 +0200

    fix sql script

---
+++ auto-aid-adviser-main/src/test/resources/data-h2-test.sql
@@ -0,0 +1,17 @@
+/* truncate table image;
+truncate table work_time;
+truncate table business_has_service;
+truncate table business;
+truncate table service;
+truncate table service_type;
+truncate table business_type;
+
+truncate table user_car;
+truncate table car_model;
+truncate table car_brand;
+truncate table type_car;
+
+truncate table simple_usr;
+truncate table business_usr;
+truncate table adviser_usr;
+ */
