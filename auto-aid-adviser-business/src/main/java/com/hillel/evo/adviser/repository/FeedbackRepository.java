7371a70ebe5aa4fc9d382cd7e48e602417b8d199
friman1488@gmail.com <friman1488@gmail.com>
2020-01-06 22:39:16 +0200

    Update FeedbackRepository

--- auto-aid-adviser-business/src/main/java/com/hillel/evo/adviser/repository/FeedbackRepository.java
+++ auto-aid-adviser-business/src/main/java/com/hillel/evo/adviser/repository/FeedbackRepository.java
@@ -17,10 +17,10 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
     Optional<Feedback> findByBusinessIdAndSimpleUserId(Long businessId, Long simpleUserId);
 
     @Query(value = "select fb from  Feedback fb join fetch fb.business b join fetch fb.simpleUser su where b.id=:businessId",
-            countQuery = "select count(fb) from Feedback fb")
+            countQuery = "select count(fb) from Feedback fb where fb.business.id =:businessId ")
     Page<Feedback> findByBusinessId(Long businessId, Pageable pageable);
 
     @Query(value = "select fb from Feedback fb join fetch fb.business b join fetch fb.simpleUser su where su.id=:simpleUserId order by fb.createDate desc ",
-            countQuery = "select count (fb) from  Feedback  fb")
+            countQuery = "select count(fb) from Feedback fb where fb.simpleUser.id =:simpleUserId")
     Page<Feedback> findBySimpleUserId(Long simpleUserId, Pageable pageable);
 }
